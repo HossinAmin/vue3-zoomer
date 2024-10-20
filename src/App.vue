beeb6a51c704de821a22c71cc5e5f4fb4774525b
HossinAmin <hossinamin70@gmail.com>
2024-10-21 00:33:30 +0300

    add multi click to move zoom component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex h-screen w-screen items-center justify-center">
-    <ZoomMapWrapper class="h-[32rem] w-[32rem]" :src="red" />
+    <ZoomMapWrapper class="h-[30rem] w-[30rem]" :src="red" :zoom-scale="5" />
   </div>
 </template>
 
 <script setup lang="ts">
 import red from "./assets/images/red.jpg";
 import "./assets/css/main.css";
-import DragZoomImg from "./components/DragZoomImg.vue";
+import ZoomMapWrapper from "./components/Wrapper/ZoomMapWrapper.vue";
 </script>
